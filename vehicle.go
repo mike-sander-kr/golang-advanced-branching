24cd74b66e5295fcb946c7d1c6e873ca6b9879b8
Mike Sander <mike.sander@kroger.com>
2022-10-04 15:28:53 -0400

    Update vehicle.go

--- vehicle.go
+++ vehicle.go
@@ -1,50 +1,52 @@
 package main
 
 import (
-
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 type vehicle interface {
-
 }
 
 type car struct {
-	model		string
-	make		string
-	typeVehicle	string
+	model       string
+	make        string
+	typeVehicle string
 }
 
 type truck struct {
-	model		string
-	make		string
-	typeVehicle	string
+	model       string
+	make        string
+	typeVehicle string
 }
 
 type bike struct {
-	model		string
-	make		string
+	model string
+	make  string
 }
 
-
-type feedbackResult struct{
-	feedbackTotal		int
-	feedbackPositive	int
-	feedbackNegative	int
-	feedbackNeutral		int
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral  int
 }
 
 var vehicleResult map[string]feedbackResult
 var inventory []vehicle
 
 // Values array for the feedback.json file
-type Values struct{
-	Models	[]Model `json:"values"`
+type Values struct {
+	Models []Model `json:"values"`
 }
 
 // Model array for the feedback.json file
-type Model struct{
-	Name		string `json:"model"`
-	Feedback 	[]string `json:"feedback"`
+type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
 }
 
 type rating float32
@@ -58,7 +60,6 @@ const (
 )
 
 func init() {
-
 
 	inventory = []vehicle{
 		bike{"FTR 1200", "Indian"},
@@ -80,11 +81,10 @@ func init() {
 func main() {
 
 	// Generate ratings for the different vehicles
-	
+	generateRating()
 	// Print ratings for the different vehicles
 }
 
-/*
 func readJSONFile() Values {
 	jsonFile, err := os.Open("feedback.json")
 
@@ -100,4 +100,39 @@ func readJSONFile() Values {
 
 	return content
 }
-*/
+
+func generateRating(){
+	f := readJOSONFile()
+
+	for _, v := range f.Models {
+		var vehResult feedbackResult
+		var vehRating rating
+		for _, msg:= range f.Feedback {
+			if text := strings.Split(msg, " "), length(text)>5{
+				vehRating = 5.0
+				vehResult.feedbackTotal ++
+				for _, word := range text{
+					switch s:=strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"), s{
+					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
+						vehRating += extraPositive
+					case "help", "helpful", "thanks", "thank you", "happy":
+						vehRating += positive
+					case "not helpful", "sad", "angry", "improve", "annoy":
+						vehRating += negative
+					case "pathetic", "bad", "worse", "unfortunately", "agitated", "frustrated":
+						vehRating += extraNegative
+					}
+				}
+				switch{
+				case vehRating > 8.0:
+					vehResult.feedbackPositive++
+				case vehRating >= 4.0 && vehRating <= 8.0:
+					vehResult.feedbackNeutral++
+				case vehRating < 4.0:
+					vehResult.feedbackNegative++
+				}
+				vehicleResult[vName] = vehResult
+			}
+		}
+	}
+}
