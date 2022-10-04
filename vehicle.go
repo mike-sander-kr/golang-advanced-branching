d6de7f6ebd02a9a80d1d7cdd57b317c89726aa87
Mike Sander <mike.sander@kroger.com>
2022-10-04 16:53:15 -0400

    Update vehicle.go

--- vehicle.go
+++ vehicle.go
@@ -101,18 +101,19 @@ func readJSONFile() Values {
 	return content
 }
 
-func generateRating(){
-	f := readJOSONFile()
+func generateRating() {
+	f := readJSONFile()
 
 	for _, v := range f.Models {
 		var vehResult feedbackResult
 		var vehRating rating
-		for _, msg:= range f.Feedback {
-			if text := strings.Split(msg, " "), length(text)>5{
-				vehRating = 5.0
-				vehResult.feedbackTotal ++
-				for _, word := range text{
-					switch s:=strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"), s{
+		for _, msg := range v.Feedback {
+			
+			if text := strings.Split(msg, " "); len(text)>=5 {
+				vehRating=5.0
+				vehResult.feedbackTotal++
+				for _, word := range text {
+					switch s := strings.Trim(strings.ToLower(word), " .!?\t\n\r"); s {
 					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
 						vehRating += extraPositive
 					case "help", "helpful", "thanks", "thank you", "happy":
@@ -123,7 +124,7 @@ func generateRating(){
 						vehRating += extraNegative
 					}
 				}
-				switch{
+				switch {
 				case vehRating > 8.0:
 					vehResult.feedbackPositive++
 				case vehRating >= 4.0 && vehRating <= 8.0:
@@ -131,8 +132,8 @@ func generateRating(){
 				case vehRating < 4.0:
 					vehResult.feedbackNegative++
 				}
-				vehicleResult[vName] = vehResult
 			}
 		}
+		vehicleResult[v.Name]=vehResult
 	}
 }
